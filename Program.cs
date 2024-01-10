04056801b4182b1335bd5f356ba7873c65c8e1be
suriyaprakash2512 <suriyaprakash.r@mphasis.com>
2024-01-10 12:05:01 +0530

    insertion

--- Program.cs
+++ Program.cs
@@ -4,3 +4,9 @@ for (int i = 1; i <= 10; i++)
 {
     Console.WriteLine($"{num}*{i}=\t{(num * i)}");
 }
+Console.WriteLine("table 6");
+num = 6;
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"{num}*{i}=\t{(num * i)}");
+}
